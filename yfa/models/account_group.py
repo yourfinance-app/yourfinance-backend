96fbf339098386f927056bae746ff9a670a1dd77
Fahim Ali Zain <fahimalizain@gmail.com>
2022-02-07 19:37:58 +0530

    fix: AccountGroup title type

--- yfa/models/account_group.py
+++ yfa/models/account_group.py
@@ -1,10 +1,10 @@
 import uuid
-from sqlmodel import SQLModel, Field, Column
+from sqlmodel import SQLModel, Field, Column, String
 from yfa.database import user_registry
 
 
 class AccountGroupBase(SQLModel, registry=user_registry):
-    title: str = Field(sa_column=Column(unique=True))
+    title: str = Field(sa_column=Column("title", String(50), unique=True))
 
 
 class AccountGroup(AccountGroupBase, table=True):
